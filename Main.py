8cfbd4b2db53088daac5758fe31c4ec43f197e46
Ivanspus <klim2006.06@mail.ru>
2025-02-12 12:44:56 +0300

    f

---
+++ Main.py
@@ -0,0 +1,119 @@
+from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import text
+
+
+DATABASE_URL = "postgresql://postgres:3891123@db:5432/my_db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+app = FastAPI(debug=True)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    notes = Column(String, nullable=True)
+Base.metadata.create_all(engine)
+
+class UserCreate(BaseModel):
+    name: str
+    phone: str
+    notes: Optional[str] = None
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    phone: str
+    notes: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_new_user(db: Session, user_data: UserCreate):
+    new_user = User(
+        name=user_data.name,
+        phone=user_data.phone,
+        notes=user_data.notes
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def get_users(db: Session):
+    return db.query(User).all()
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def update_user(db: Session, user_id: int, user_data: UserCreate):
+    user = get_user_by_id(db, user_id)
+    if not user:
+        return None
+    user.name = user_data.name
+    user.phone = user_data.phone
+    user.notes = user_data.notes
+    db.commit()
+    db.refresh(user)
+    return user
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        return None
+    db.delete(user)
+    db.commit()
+    return user
+
+def delete_all_users(db: Session):
+    db.query(User).delete()
+    db.commit()
+    db.execute(text("ALTER SEQUENCE users_id_seq RESTART WITH 1;"))
+    db.commit()
+    return {"message": "Все пользователи успешно удалены"}
+
+@app.post("/users/create", response_model=UserResponse)
+def create_user_route(user_data: UserCreate, db: Session = Depends(get_db)):
+    return create_new_user(db=db, user_data=user_data)
+
+@app.get("/users/get_all", response_model=list[UserResponse])
+def get_users_route(db: Session = Depends(get_db)):
+    return get_users(db=db)
+
+@app.get("/users/get_by_id", response_model=UserResponse)
+def get_user_by_id_route(user_id: int, db: Session = Depends(get_db)):
+    user = get_user_by_id(db=db, user_id=user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    return user
+
+@app.put("/users/update_user_by_id", response_model=UserResponse)
+def update_user_route(user_id: int, user_data: UserCreate, db: Session = Depends(get_db)):
+    updated_user = update_user(db=db, user_id=user_id, user_data=user_data)
+    if not updated_user:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    return updated_user
+
+@app.delete("/users/delete_user_by_id", response_model=UserResponse)
+def delete_user_route(user_id: int, db: Session = Depends(get_db)):
+    deleted_user = delete_user(db=db, user_id=user_id)
+    if not deleted_user:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    return deleted_user
+
+@app.delete("/users/delete_all_users", response_model=dict)
+def delete_all_users_route(db: Session = Depends(get_db)):
+    return delete_all_users(db=db)
